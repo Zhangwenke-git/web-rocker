d84d160c5b139d54b92ab334485868ac57531b8a
Zhangwenke-git <2499864525@qq.com>
2021-08-29 11:10:00 +0800

    删除WEB一级菜单

--- core/exceptions.py
+++ core/exceptions.py
@@ -13,6 +13,14 @@ class BaseError(APIException):
 class DefiendError(BaseError):
     """默认异常，自定义使用，返回状态-1，响应状态400"""
 
-    def __init__(self, message=None, code=-1, success=False, result=None):
+    def __init__(self, message="发生异常", code=-1, success=False, result=None):
         detail = {'code': code, 'message': message, 'success': success, 'result': result}
         super(DefiendError, self).__init__(detail)
+
+
+class DefiendSuccess(BaseError):
+    """默认异常，自定义使用，返回状态0，响应状态200"""
+
+    def __init__(self, message="响应成功", code=0, success=True, result=None):
+        detail = {'code': code, 'message': message, 'success': success, 'result': result}
+        super(DefiendSuccess, self).__init__(detail,status_code=status.HTTP_200_OK)
